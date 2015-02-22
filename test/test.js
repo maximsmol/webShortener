64efb5bb134be19e54da0bbc4156b93a50e21247
maximsmol <maximsmol@gmail.com>
2015-02-22 13:02:14 +0300

    v0.1.0

---
+++ test/test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//
+// Standard libraries
+var fs = require('fs');
+var assert = require('assert');
+
+//
+//
+var webShortener = require('../lib/webShortener');
+
+//
+// Mocha
+var describe = global.describe;
+var it = global.it;
+
+
+//
+// Main code
+//
+
+//
+// Tests
+describe('Testing', function test()
+{
+	it('Result should be ideal', function test()
+	{
+		var htmlFile = './test/assets/index.html';
+		var cssFile  = './test/assets/main.css';
+		var jsFile   = './test/assets/main.js';
+
+		webShortener([htmlFile], [cssFile], [jsFile]);
+
+		assert.strictEqual(
+			fs.readFileSync(htmlFile+'.ideal').toString(),
+			fs.readFileSync(htmlFile+'.parsed').toString()
+		);
+		assert.strictEqual(
+			fs.readFileSync(cssFile+'.ideal').toString(),
+			fs.readFileSync(cssFile+'.parsed').toString()
+		);
+		assert.strictEqual(
+			fs.readFileSync(jsFile+'.ideal').toString(),
+			fs.readFileSync(jsFile+'.parsed').toString()
+		);
+	});
+});
