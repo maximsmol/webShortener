3ba0f0681e8275954885e68dc352d6a5d1e5a185
maximsmol <1472826+maximsmol@users.noreply.github.com>
2015-02-22 13:02:14 +0300

    v0.1.0

---
+++ test/assets/main.js
@@ -0,0 +1,3 @@
+document.getElementById('/* webname #reallyLongIdWow-hyphen_andStuff */');
+document.getElementsByClassName('/* webname .reallyLongClassNameWow-hyphen_andStuff */');
+console.log(/* usual comment */'/* webname #reallyLongIdWow-hyphen_andStuff */');
