87ade20c35ec9bca438b22a1e566399c1f48af8a
maximsmol <maximsmol@gmail.com>
2015-02-28 22:25:03 +0300

    v1.0.0

--- test/assets/main.js
+++ test/assets/main.js
@@ -1,6 +1,7 @@
 document.getElementById('/* webname #reallyLongIdWow-hyphen_andStuff */');
 document.getElementsByClassName('/* webname .reallyLongClassNameWow-hyphen_andStuff */');
 console.log(/* usual comment */'/* webname #reallyLongIdWow-hyphen_andStuff */');
-
+//COMMENT
+/* * */
 console.log('/* webname .notInHTML */');
 console.log('/* webname #notInHTML */');
--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var assert = require('assert');
 
 //
 //
-var webShortener = require('../lib/webShortener');
+var Shortener = require('../lib/webShortener');
 
 //
 // Mocha
@@ -16,47 +16,135 @@ var it = global.it;
 
 
 //
-// Main code
+// Tests
 //
 
-//
-// Tests
 describe('Output test', function output()
 {
-	it('Result should be ideal', function test()
+	it('Result should be ideal', function test(done)
 	{
 		var htmlFile = './test/assets/index.html';
 		var cssFile  = './test/assets/main.css';
 		var jsFile   = './test/assets/main.js';
 
-		webShortener([htmlFile], [cssFile], [jsFile]);
+		var shrtnr = new Shortener();
 
-		assert.strictEqual(
-			fs.readFileSync(htmlFile+'.ideal').toString(),
-			fs.readFileSync(htmlFile+'.parsed').toString()
-		);
-		assert.strictEqual(
-			fs.readFileSync(cssFile+'.ideal').toString(),
-			fs.readFileSync(cssFile+'.parsed').toString()
-		);
-		assert.strictEqual(
-			fs.readFileSync(jsFile+'.ideal').toString(),
-			fs.readFileSync(jsFile+'.parsed').toString()
-		);
+		fs.createReadStream(htmlFile).pipe(shrtnr.htmlShortener())
+		.pipe(fs.createWriteStream(htmlFile+'.parsed'))
+		.on('finish', function()
+		{
+			fs.createReadStream(cssFile).pipe(shrtnr.cssShortener())
+			.pipe(fs.createWriteStream(cssFile+'.parsed'))
+			.on('finish', function()
+			{
+				fs.createReadStream(jsFile).pipe(shrtnr.jsShortener())
+				.pipe(fs.createWriteStream(jsFile+'.parsed'))
+				.on('finish', function()
+				{
+					assert.strictEqual(
+						fs.readFileSync(htmlFile+'.ideal').toString(),
+						fs.readFileSync(htmlFile+'.parsed').toString()
+					);
+					assert.strictEqual(
+						fs.readFileSync(cssFile+'.ideal').toString(),
+						fs.readFileSync(cssFile+'.parsed').toString()
+					);
+					assert.strictEqual(
+						fs.readFileSync(jsFile+'.ideal').toString(),
+						fs.readFileSync(jsFile+'.parsed').toString()
+					);
+
+					done();
+				});
+			});
+		});
 	});
 });
 
-describe('Replacement generator test', function stress()
+describe('Replacement generator test', function generator()
 {
-	it('Result should be ideal', function test()
+	it('Result should be ideal', function test(done)
 	{
 		var htmlFile = './test/assets/generator.html';
 
-		webShortener([htmlFile], [], []);
+		var shrtnr = new Shortener();
 
-		assert.strictEqual(
-			fs.readFileSync(htmlFile+'.ideal').toString(),
-			fs.readFileSync(htmlFile+'.parsed').toString()
-		);
+		fs.createReadStream(htmlFile).pipe(shrtnr.htmlShortener())
+		.pipe(fs.createWriteStream(htmlFile+'.parsed'))
+		.on('finish', function()
+		{
+			assert.strictEqual(
+				fs.readFileSync(htmlFile+'.ideal').toString(),
+				fs.readFileSync(htmlFile+'.parsed').toString()
+			);
+
+			done();
+		});
+	});
+});
+
+describe('Testing errors', function errors()
+{
+	describe('Testing no id in map', function noId()
+	{
+		it('Should throw the correct error', function test(done)
+		{
+			var jsFile = './test/assets/noSuchId.js';
+
+			var shrtnr = new Shortener();
+
+			var s = null;
+			s = fs.createReadStream(jsFile).pipe(shrtnr.jsShortener())
+			.on('error', function(err)
+			{
+				assert.strictEqual(err, 'No such id in map: "id"');
+				done();
+			})
+			.pipe(fs.createWriteStream(jsFile+'.parsed'));
+
+			s.on('finish', done);
+		});
+	});
+
+	describe('Testing no class in map', function noClass()
+	{
+		it('Should throw the correct error', function test(done)
+		{
+			var jsFile = './test/assets/noSuchClass.js';
+
+			var shrtnr = new Shortener();
+
+			var s = null;
+			s = fs.createReadStream(jsFile).pipe(shrtnr.jsShortener())
+			.on('error', function(err)
+			{
+				assert.strictEqual(err, 'No such class in map: "class"');
+				done();
+			})
+			.pipe(fs.createWriteStream(jsFile+'.parsed'));
+
+			s.on('finish', done);
+		});
+	});
+
+	describe('Testing no such webname', function noWebname()
+	{
+		it('Should throw the correct error', function test(done)
+		{
+			var jsFile = './test/assets/noSuchWebname.js';
+
+			var shrtnr = new Shortener();
+
+			var s = null;
+			s = fs.createReadStream(jsFile).pipe(shrtnr.jsShortener())
+			.on('error', function(err)
+			{
+				assert.strictEqual(err, 'Invalid webname: "?test"');
+				done();
+			})
+			.pipe(fs.createWriteStream(jsFile+'.parsed'));
+
+			s.on('finish', done);
+		});
 	});
 });
